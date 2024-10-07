c3d0b7a4116f095294ad6e48b447d29bd62fc997
sunilaadhe <sunilaadhe62@gmail.com>
2024-10-07 10:02:19 +0530

    vat lagli

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ let recife = async() => {
       let respone =  await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`);   
       let data = await respone.json();
       console.log(data);
-      console.log(data[24]);
-      
-      one.innerHTML= data[24];
+      console.log(data.fish);
+        
+      one.innerHTML= data.fish;
       
 }
 
-input.addEventListener("input",()=>{
+input.addEventListener("input",(e)=>{
     recife()    
 })
