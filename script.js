678825357fdcad5f67429e432024cdb9462b08a7
sunilaadhe <sunilaadhe62@gmail.com>
2024-10-11 21:29:40 +0530

    completet

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 
-let input = document.querySelector("input")
+let input = document.querySelector("input");
+let one = document.querySelector("#one");
 
-let one = document.querySelector("#one")
+let recife = async () => {
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`);
+    let data = await response.json();
 
+    if (data?.meals) {
+  
+        one.innerHTML = '<div class="prnt"></div>';
+        let prnt = one.querySelector(".prnt");
 
-let recife = async() => {
-      let respone =  await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`);   
-      let data = await respone.json();
-    //   console.log(data);
-    //   console.log(data.meals[9].strArea);
-      
-        
-      if(data?.meals){
-        data?.meals.forEach(meal => {
-            console.log(meal.strMealThumb)
-            one.innerHTML +=`<div class="parant"><div class='card'>
-            <h6> <p>hello</p>${meal.strArea}</h6><br/>
-      <p>Category : ${meal.strCategory }</p>
-            <img class='img' src=${meal.strMealThumb} 
-            </div> <br/></div>`
+        data.meals.forEach(meal => {
+            prnt.innerHTML += `
+                <div class='card'>
+                    <h6><p>${meal.strCategory}</p>${meal.strArea}</h6><br/>
+                    <p>Category : ${meal.strCategory}</p>
+                    <img class='Foodimg' src=${meal.strMealThumb} alt="${meal.strMeal}">
+                </div>`;
         });
-      }
+    } else {
+        one.innerHTML = '<p>No results found</p>';
+    }
+};
 
-    
-}
-
-input.addEventListener("input",(e)=>{
-    recife()    
-})
+input.addEventListener("input", (e) => {
+    recife();
+});
